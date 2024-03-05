4ba4fb141742143e82a46b290dd233d94eac988e
Marek <marek.vitek.ml@seznam.cz>
2024-03-05 09:08:13 +0100

    commit

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('randomnumber', views.models_list)
+    path('randomnumber', views.models_list),
+    path('asas', views.post_list),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -9,4 +9,5 @@ def models_list(request):
     return HttpResponse(f"Random number {random_number}")
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    random_number = random.randint(1, 100)
+    return render(request, 'blog/post_list.html', {'var':random_number})
